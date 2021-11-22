7a81452eefe9af08daee1d053a81d8567da09e3c
HTXuser1 <82179264+HTXuser1@users.noreply.github.com>
2021-11-22 11:00:17 +0100

    Add files via upload

---
+++ melody.ino
@@ -0,0 +1,26 @@
+#include "pitches.h"
+
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+int noteDuration;
+int pauseBetweenNotes;
+
+void setup() {
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+}
+
+void loop() {
+  // no need to repeat the melody.
+}
